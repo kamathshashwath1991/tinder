52e07c1ea4aa0ccb6a21711b5843352760ebf4db
Shashwath Kamath <kamathshashwath1991@gmail.com>
2018-08-01 07:25:12 -0400

    Photo Synopsis and Photo Background created.

--- lib/main.dart
+++ lib/main.dart
@@ -138,14 +138,100 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 class ProfileCard extends StatefulWidget {
+
   @override
   _ProfileCardState createState() => _ProfileCardState();
 }
 
 class _ProfileCardState extends State<ProfileCard> {
+
+  Widget _buildBackground(){
+    return new Image.asset(
+        'assets/a.jpeg',
+      fit: BoxFit.cover,
+    );
+  }
+
+  Widget _buildProfileSynopsis(){
+    return new Positioned(
+      left: 0.0,
+      right: 0.0,
+      bottom: 0.0,
+      child: new Container(
+        decoration: new BoxDecoration(
+          gradient: new LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+
+            colors: [
+              Colors.transparent,
+              Colors.black.withOpacity(0.8)
+            ]
+          )
+        ),
+        padding: const EdgeInsets.all(24.0),
+        child: new Row(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            new Expanded(
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  new Text(
+                    'First Last',
+                    style: new TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0
+                    ),
+                  ),
+                  new Text(
+                    'some description',
+                    style: new TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            new Icon(
+              Icons.info,
+              color: Colors.white,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: new BoxDecoration(
+        borderRadius: new BorderRadius.circular(10.0),
+        boxShadow: [
+          new BoxShadow(
+            color: const Color(0x11000000),
+            blurRadius: 5.0,
+            spreadRadius: 2.0,
+          )
+        ]
+      ),
+      child: ClipRRect(
+        borderRadius: new BorderRadius.circular(10.0),
+        child: new Material(
+          child: new Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              _buildBackground(),
+              _buildProfileSynopsis(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
 
